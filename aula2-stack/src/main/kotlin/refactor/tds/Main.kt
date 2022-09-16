a8dcac4230e620065f1c3058a8ee5c23201575a3
joaquimdferreira <theicyshad0w@gmail.com>
2022-09-16 17:52:48 +0100

    Revision of stack from lesson 2

---
+++ aula2-stack/src/main/kotlin/refactor/tds/Main.kt
@@ -0,0 +1,15 @@
+package refactor.tds
+
+
+fun main() {
+    val stk = makeStack<String>()
+    stk.push("ISEL")
+    stk.push("TDS")
+    println(stk.peek()) // TDS
+    while( !stk.isEmpty() ) {
+        println( stk.pop() ) // TDS ISEL
+    }
+    stk.peek() // throws NoSuchElementException
+}
+
+fun <T> makeStack() = MyStack<T>()
--- aula2-stack/src/main/kotlin/refactor/tds/Stack.kt
+++ aula2-stack/src/main/kotlin/refactor/tds/Stack.kt
@@ -1,25 +1,8 @@
 package refactor.tds
 
-import java.util.NoSuchElementException
-
-class Node<T>(val value: T,val next: Node<T>? = null)
-
-class Stack<T>() {
-    private var head: Node<T>? = null
-    fun push(value: T) {
-        head = if(head == null) Node(value, null)
-        else Node(value, head)
-    }
-
-    fun pop(): T?{
-        if(head == null)return null
-        val curr = head
-        head = head?.next
-        return curr!!.value
-    }
-
-    fun isEmpty() = head == null
-
-    fun peek() = head?.value ?: throw NoSuchElementException()
-
+interface Stack<T> {
+    fun push(value : T)
+    fun pop(): T
+    fun isEmpty(): Boolean
+    fun peek(): T
 }
